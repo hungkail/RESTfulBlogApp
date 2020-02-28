8e1c76159b76ca098fdcb72206d542b42403455d
Hung Kai Liao <hungkail@bekeley.edu>
2020-02-28 10:56:34 -0800

    finish set up

--- app.js
+++ app.js
@@ -3,5 +3,18 @@ const bodyParser = require("body-parser"),
       express    = require("express"),
       app        = express();
 const url = 'mongodb://127.0.0.1:27017/restful_blog_app';
-const port = 3000;
+const port = 4000;
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+// title
+// image
+// body
+// created
+
+
+
+app.listen(port, function () {
+    console.log("SERVER IS RUNNING!");
+});

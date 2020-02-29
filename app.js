254fb392d624c8774c1e0387ccb9aa016d1bd829
Hung Kai Liao <hungkail@bekeley.edu>
2020-02-29 15:01:27 -0800

    finished

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const bodyParser = require("body-parser"),
-      mongoose   = require("mongoose"),
-      express    = require("express"),
-      app        = express();
+const bodyParser       = require("body-parser"),
+      methodOverride   = require("method-override"),
+      expressSanitizer = require("express-sanitizer"),
+      mongoose         = require("mongoose"),
+      express          = require("express"),
+      app              = express();
 const url = 'mongodb://127.0.0.1:27017/restful_blog_app';
 const port = 4000;
 // APP CONFIG
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+// Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
+// by default, you need to set 'findAndModify()' to false.
+mongoose.set('useFindAndModify', false);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -17,6 +24,7 @@ const blogSchema = new mongoose.Schema({
     body:  String,
     created: {type: Date, default: Date.now}
 });
+
 const Blog = mongoose.model("Blog", blogSchema);
 
 // Blog.create({
@@ -31,6 +39,7 @@ app.get("/", function (req, res) {
     res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function (req, res) {
     Blog.find({}, function (err, blogs) {
         if (err) {
@@ -39,6 +48,68 @@ app.get("/blogs", function (req, res) {
             res.render("index", {blogs: blogs});
         }
     });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function (req, res) {
+   res.render("new");
+});
+// CREATE ROUTE
+app.post("/blogs", function (req, res) {
+    // create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function (err, newBlog) {
+        if(err){
+            res.render("new");
+        } else {
+            // redirect to the index
+            res.redirect("/blogs");
+        }
+    })
+});
+
+// SHOW ROUTE
+app.get("/blogs/:id", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    })
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function (req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/"+req.params.id);
+        }
+    })
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function (req, res) {
+    Blog.findByIdAndRemove(req.params.id, function (err) {
+        if(err){
+            console.log(err);
+        }
+        res.redirect("/blogs");
+    })
 });
 
 // title

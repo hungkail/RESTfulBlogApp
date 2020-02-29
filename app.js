de312a30450e0881dcdbeec2bce3c61efb26bc51
Hung Kai Liao <hungkail@bekeley.edu>
2020-02-29 08:06:06 -0800

    add header, footer, semantic-ui, navbar

--- app.js
+++ app.js
@@ -4,10 +4,43 @@ const bodyParser = require("body-parser"),
       app        = express();
 const url = 'mongodb://127.0.0.1:27017/restful_blog_app';
 const port = 4000;
+// APP CONFIG
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body:  String,
+    created: {type: Date, default: Date.now}
+});
+const Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=660&q=80",
+//     body: "HELLO THIS IS A BLOG POST!"
+// });
+
+// RESTFUL ROUTES
+
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
 // title
 // image
 // body
